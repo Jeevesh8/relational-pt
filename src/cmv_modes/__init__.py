37f4ef202c7afa997cc923598216f9c5e02bcd27
Jeevesh8 <creativityinczenyoga@gmail.com>
2021-07-08 21:35:44 +0530

    trying dynamic batching

--- src/cmv_modes/__init__.py
+++ src/cmv_modes/__init__.py
@@ -1,6 +1,6 @@
 import os, shlex, glob
 import random
-from typing import List, Dict
+from typing import List, Dict, Tuple, Optional
 from collections import namedtuple
 from functools import partial
 
@@ -46,38 +46,123 @@ def data_generator(file_list: List[str]):
         yield elem
 
 
+def _create_min_max_boundaries(max_length: int,
+                               min_boundary: int =256,
+                               boundary_scale: float =1.1) -> Tuple[List[int], List[int]]:
+    """Forms buckets. All samples with sequence length between the boundaries of a single bucket 
+    are to be padded to the same length. 
+    Args:
+        max_length:     The max_length of any tokenized sequence that will be input to model.
+        min_boundary:   The largest sequence length that can be accomodated in the first bucket.
+        boundary_scale: The factor by which previous bucket's max length should be multiplied to get
+                        the max length for next bucket.
+    Returns:
+        2 Lists, ``buckets_min`` and ``buckets_max`` the i-th bucket corresponds to sequence lengths 
+        from buckets_min[i-1] to buckets_max[i-1].
+    """
+    bucket_boundaries = []
+    x = min_boundary
+    while x < max_length:
+        bucket_boundaries.append(x)
+        x = max(x + 1, int(x * boundary_scale))
+
+    buckets_min = [0] + bucket_boundaries
+    buckets_max = bucket_boundaries + [max_length + 1]
+    return buckets_min, buckets_max
+
+
+def _batch_examples(dataset: tf.Dataset, batch_size: int, max_length: int, min_boundary: Optional[int]=256) -> tf.Dataset:
+    """Dynamically batches the samples in dataset. Buckets of similar sample length samples are 
+    made. Batches are made from elements in a bucket and each sequence is padded to the max length of
+    sample in the batch.
+    Args:
+        dataset:      A tensorflow dataset to be dynamically batched.
+        batch_size:   Number of tokens expected in a single batch. That is the expected sum of 
+                      all tokens in all samples in a batch
+        max_length:   Max. sequence length any sample from dataset. max_length <= batch_size.
+        min_boundary: The max. length of any sequence in the 1-st bucket.
+    Returns:
+        A tensorflow dataset that returns dynamically batched and padded sample.
+    """
+    def get_sample_len(*args):
+        return tf.shape(args[1])[0]
+    
+    if batch_size<max_length:
+        raise ValueError("The expected number of tokens in a single batch(batch_size) must be \
+                         >= max_length of any sequence in dataset. Got batch_size, max_length as:",
+                         (batch_size, max_length))
+    actual_max_len = 0
+    for elem in dataset:
+        sample_len = get_sample_len(*elem)
+        if actual_max_len<sample_len:
+            actual_max_len = sample_len
+    
+    if actual_max_len > max_length:
+        raise AssertionError("Found sequence with longer length than specified: ", max_length,
+                             "in dataset. Actual max length:", actual_max_len)
+    
+    buckets_min, buckets_max = _create_min_max_boundaries(max_length, min_boundary=min_boundary)
+    bucket_batch_sizes = [int(batch_size) // x for x in buckets_max]
+    bucket_batch_sizes = tf.constant(bucket_batch_sizes, dtype=tf.int64)
+
+    def example_to_bucket_id(*args) -> int:
+        """Returns the index of bucket that the input specified in *args
+        falls in."""
+        seq_length = get_sample_len(*args)
+
+        conditions_c = tf.logical_and(
+            tf.less_equal(buckets_min, seq_length), tf.less(seq_length,
+                                                        buckets_max))
+        bucket_id = tf.reduce_min(tf.where(conditions_c))
+        return bucket_id
+
+    def window_size_fn(bucket_id: int) -> int:
+        """Returns the size of batch for the bucket at index bucket_id."""
+        return bucket_batch_sizes[bucket_id]
+
+    def batching_fn(bucket_id: int, grouped_dataset: tf.Dataset) -> tf.Dataset:
+        """Batches a subset of a dataset, provided in grouped_dataset. Each element is padded 
+        to the max. sequence length in the batch. Batches in same bucket may be padded to different 
+        lengths.
+        """
+        bucket_batch_size = window_size_fn(bucket_id)
+
+        return grouped_dataset.padded_batch(bucket_batch_size,
+                                            padded_shapes=([None],[None],[None],[None]),
+                                            padding_values=(None, *tuple(data_config["pad_for"].values())))
+    return dataset.apply(
+        tf.data.experimental.group_by_window(
+          key_func=example_to_bucket_id,
+          reduce_func=batching_fn,
+          window_size=None,
+          window_size_func=window_size_fn)).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+
 def get_dataset(file_list: List[str]):
     def callable_gen():
         nonlocal file_list
         for elem in data_generator(file_list):
             yield elem
 
-    return (tf.data.Dataset.from_generator(
-        callable_gen,
-        output_signature=(
-            tf.TensorSpec(shape=(), dtype=tf.string, name="filenames"),
-            tf.TensorSpec(shape=(None),
-                          dtype=tf.int32,
-                          name="tokenized_threads"),
-            tf.TensorSpec(shape=(None),
-                          dtype=tf.int32,
-                          name="comp_type_labels"),
-            tf.TensorSpec(shape=(None, 3),
-                          dtype=tf.int32,
-                          name="refers_to_and_type"),
-        ),
-    ).padded_batch(
-        ft_config["batch_size"],
-        padded_shapes=(
-            [],
-            [stable_config["max_len"]],
-            [stable_config["max_len"]],
-            [stable_config["max_len"], 3],
-        ),
-        padding_values=(None, *tuple(data_config["pad_for"].values())),
-    ).batch(stable_config["num_devices"],
-            drop_remainder=True).map(convert_to_named_tuple))
-
+    sample_wise_dataset =  tf.data.Dataset.from_generator(callable_gen,
+                                                          output_signature=(
+                                                            tf.TensorSpec(shape=(), dtype=tf.string, name="filenames"),
+                                                            tf.TensorSpec(shape=(None),
+                                                                        dtype=tf.int32,
+                                                                        name="tokenized_threads"),
+                                                            tf.TensorSpec(shape=(None),
+                                                                        dtype=tf.int32,
+                                                                        name="comp_type_labels"),
+                                                            tf.TensorSpec(shape=(None, 3),
+                                                                        dtype=tf.int32,
+                                                                        name="refers_to_and_type"),
+                                                            ),
+                                                          )
+    
+    dataset = _batch_examples(sample_wise_dataset, 8194, 4096)
+    dataset = dataset.map(convert_to_named_tuple)
+    
+    return dataset
 
 def get_op_wise_split(filelist: List[str]) -> Dict[str, List[str]]:
     """Splits up filelist into groups, each of which corresponds to threads with
